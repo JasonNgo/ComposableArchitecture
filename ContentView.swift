2cff39b202cbd3e242f0069edf443198cfe9701b
Jason Ngo <jason.n.ngo@gmail.com>
2024-01-10 14:29:21 -0500

    [feat] Create UI for CounterView

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                NavigationLink(destination: EmptyView()) {
+                NavigationLink(destination: CounterView()) {
                     Text("Counter Demo")
                 }
                 NavigationLink(destination: EmptyView()) {
@@ -13,5 +13,35 @@ struct ContentView: View {
             }
             .navigationTitle("State Management")
         }
+    }
+}
+
+struct CounterView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {}) {
+                    Text("-")
+                }
+                Text("0")
+                Button(action: {}) {
+                    Text("+")
+                }
+            }
+
+            Button {
+
+            } label: {
+                Text("Is this prime?")
+            }
+
+            Button {
+
+            } label: {
+                Text("What is the 0th prime?")
+            }
+        }
+        .font(.title)
+        .navigationTitle("Counter Demo")
     }
 }
