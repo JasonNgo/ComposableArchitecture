64161bca217006d4a3900e654f0938ac750229f2
Jason Ngo <jason.n.ngo@gmail.com>
2024-01-10 14:25:33 -0500

    [feat] Add navigation links for modules

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                NavigationLink(destination: EmptyView()) {
+                    Text("Counter Demo")
+                }
+                NavigationLink(destination: EmptyView()) {
+                    Text("Favourite Primes")
+                }
+            }
+            .navigationTitle("State Management")
         }
     }
 }
